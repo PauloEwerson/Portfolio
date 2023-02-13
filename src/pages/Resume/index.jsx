0291a81301977f51b308576ebec14284b752515e
Ewerson Martins <ewersonmrt@gmail.com>
2023-02-13 07:38:51 -0300

    fix: Corrige link para a página de Projetos no Navbar e link do arquivo do curriculo

--- src/pages/Resume/index.jsx
+++ src/pages/Resume/index.jsx
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import styles from './styles.module.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = "https://github.com/PauloEwerson/Portfolio/tree/main/src/Assets/curriculo_ewerson.pdf";
+const resumeLink = "https://raw.githubusercontent.com/PauloEwerson/Portfolio/blob/main/src/Assets/curriculo/curriculo_ewerson.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
