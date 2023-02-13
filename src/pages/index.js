83f3d3e4a89dc600f81e2f624cd7b77302a7943e
Ewerson Martins <ewersonmrt@gmail.com>
2023-02-13 05:26:37 -0300

    feat: Implementa página de Projetos com três aplicações iniciais.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from '../components/Navbar';
 export { default as Footer } from '../components/Footer';
 export { default as Home } from './Home/Hero';
 export { default as About } from './About';
+export { default as Projects } from './Projects';
