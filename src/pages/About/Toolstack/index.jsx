af5b2015bc31e63bd50391e2b9960c02767a012c
Ewerson Martins <ewersonmrt@gmail.com>
2023-03-04 13:57:15 -0300

    feat: Adiciona nome das tecnologias e skills ao clicar no ícone

--- src/pages/About/Toolstack/index.jsx
+++ src/pages/About/Toolstack/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   AiFillGithub,
@@ -12,22 +12,72 @@ import {
 import styles from './styles.module.css';
 
 function Toolstack() {
+  const [clickedTech, setClickedTech] = useState("");
+
+  const handleIconClick = (toolName) => {
+    if (clickedTech === toolName) {
+      setClickedTech("");
+    } else {
+      setClickedTech(toolName);
+    }
+  };
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className={styles.techIcons}>
+      <Col
+        xs={4}
+        md={2}
+        className={styles.techIcons}
+        onClick={() => handleIconClick("Linux")}
+      >
         <SiLinux />
+        {clickedTech === "Linux" && (
+          <div className={styles.toolName}>{"Linux"}</div>
+        )}
       </Col>
-      <Col xs={4} md={2} className={styles.techIcons}>
+      <Col 
+        xs={4}
+        md={2}
+        className={styles.techIcons}
+        onClick={() => handleIconClick("VSCode")}
+      >
         <SiVisualstudiocode />
+        {clickedTech === "VSCode" && (
+          <div className={styles.toolName}>{"VSCode"}</div>
+        )}
       </Col>
-      <Col xs={4} md={2} className={styles.techIcons}>
+      <Col
+        xs={4}
+        md={2}
+        className={styles.techIcons}
+        onClick={() => handleIconClick("Insomnia")}
+      >
         <SiInsomnia />
+        {clickedTech === "Insomnia" && (
+          <div className={styles.toolName}>{"Insomnia"}</div>
+        )}
       </Col>
-      <Col xs={4} md={2} className={styles.techIcons}>
+      <Col
+        xs={4}
+        md={2}
+        className={styles.techIcons}
+        onClick={() => handleIconClick("Github")}
+      >
         <AiFillGithub />
+        {clickedTech === "Github" && (
+          <div className={styles.toolName}>{"Github"}</div>
+        )}
       </Col>
-      <Col xs={4} md={2} className={styles.techIcons}>
+      <Col
+        xs={4}
+        md={2}
+        className={styles.techIcons}
+        onClick={() => handleIconClick("Figma")}
+      >
         <SiFigma />
+        {clickedTech === "Figma" && (
+          <div className={styles.toolName}>{"Figma"}</div>
+        )}
       </Col>
     </Row>
   );
