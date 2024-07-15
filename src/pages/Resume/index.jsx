4fbcb1f762ce939cee570d26d67b7e1133edd2a6
PauloEwerson <ewersonmrt@gmail.com>
2024-07-15 19:38:11 -0300

    Modifica Curriculo

--- src/pages/Resume/index.jsx
+++ src/pages/Resume/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../../components/Particle";
-import pdf from "../../Assets/curriculo/curriculo_ewerson.pdf";
+import pdf from "../../Assets/curriculo/Ewerson_Curriculo.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
